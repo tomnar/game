0c25acd78301552824836fd4fc23506cd2f60e9d
tomnar <tomquast89@gmail.com>
2017-08-08 21:02:47 +0200

    Paths have to be relative to work

--- Client/src/views/404/404.component.ts
+++ Client/src/views/404/404.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'page-not-found',
-    templateUrl: './404.component.html',
-    styleUrls: ['./404.component.scss']
+    templateUrl: '404.component.html',
+    styleUrls: ['404.component.scss']
 })
 export class PageNotFoundComponent {
     title = '404 - Page not found';
--- Client/src/views/home/home.component.ts
+++ Client/src/views/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root2',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    templateUrl: 'home.component.html',
+    styleUrls: ['home.component.scss']
 })
 export class HomeComponent {
     title = 'home';
